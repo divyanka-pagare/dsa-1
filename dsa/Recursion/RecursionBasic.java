1fd0a10e28b6e032b2e619dcf31d97fcaa3b2822
dell <pagaredivyanka@gmail.com>
2026-07-16 19:26:48 +0530

    qs

--- dsa/Recursion/RecursionBasic.java
+++ dsa/Recursion/RecursionBasic.java
@@ -19,11 +19,86 @@ public class RecursionBasic {
         printInc(n-1);
         System.out.print(n+" ");
     }
+
+
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int fnm1 = fact(n-1);
+        int fn = n * fnm1;
+        return fn;
+    }
+
+    public static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int snm1 = sum(n-1);
+        int sn = n + snm1;  
+        return sn;
+    }
+
+    public static int fib(int n) {   // time complexity = O(2^n)
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int fnm1 = fib(n-1);
+        int fnm2 = fib(n-2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    }
+
+    public static boolean isSorted(int arr[], int i) {
+        if(i == arr.length-1) {
+            return true;
+        }
+
+        if (arr[i] > arr[i+1]) {
+            return false;
+        }
+        return isSorted(arr, i+1);
+    }
+
+
+    public static int firstOcc(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+
+        if (arr[i] == key) {
+            return i;
+        }
+        return firstOcc(arr, key, i+1);
+    }
+
+    public static int lastOccurence(int arr[], int key, int i) {  // first look forward then compare with self
+        if(i == arr.length) {
+            return -1;
+        }
+
+        int isFound = lastOccurence(arr, key, i+1);
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+        return isFound;
+    }
+
+
     public static void main(String args[]) {
-        int n = 10;
+        // int n = 11;
 
         // printDec(n);
-        printInc(n);
+        // printInc(n);
+        // System.out.println(fact(n));
+        // System.out.println(sum(n));
+        // System.out.println(fib(n));
 
+        // int arr[] = {1, 2, 3, 4, 5};  //true
+        int arr[] = {1, 2, 11, 8, 6, 11, 5};   //false
+        // int arr[] ={11};
+        // System.out.println(isSorted(arr, 0));
+        // System.out.println(firstOcc(arr, 11, 0));
+        System.out.println(lastOccurence(arr, 11, 0));
     }
 }
