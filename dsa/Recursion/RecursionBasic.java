85adcf10291ed6a01f59b82eb40f04b5dad3cac0
dell <pagaredivyanka@gmail.com>
2026-07-16 18:12:50 +0530

    ans in single line

--- dsa/Recursion/RecursionBasic.java
+++ dsa/Recursion/RecursionBasic.java
@@ -4,20 +4,20 @@ public class RecursionBasic {
 
     public static void printDec(int n) {
         if (n == 1) {
-            System.out.println(n);
+            System.out.print(n+" ");
             return;
         }
-        System.out.println(n+" ");
+        System.out.print(n+" ");
         printDec(n-1);
     }
 
     public static void printInc(int n) {
         if (n == 1) {
-            System.out.println(n);
+            System.out.print(n+" ");
             return;
         }
         printInc(n-1);
-        System.out.println(n+" ");
+        System.out.print(n+" ");
     }
     public static void main(String args[]) {
         int n = 10;
