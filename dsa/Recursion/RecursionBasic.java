110e90214ae49084bf76158006491b49eddec465
dell <pagaredivyanka@gmail.com>
2026-07-17 16:25:52 +0530

    recursion 2

--- dsa/Recursion/RecursionBasic.java
+++ dsa/Recursion/RecursionBasic.java
@@ -84,8 +84,92 @@ public class RecursionBasic {
         return isFound;
     }
 
+    public static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        
+        // int xnm1 = power(x, n-1);
+        // int xn = x * xnm1;
+        // return xn;
+
+        return x * power(x, n-1);
+    }
+
+
+    //tilingProblem
+    public static int tilingProblem(int n) {
+        //base case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        //kaam
+        // //vertical choice
+        // int fnm1 = tilingProblem(n-1);
+
+        // //horizontal choice
+        // int fnm2 = tilingProblem(n-2);
+
+        return tilingProblem(n-1) + tilingProblem(n-2);
+    }
+
+    //remove duplicates
+    public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+        if(idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+
+        //kaam
+        char currChar = str.charAt(idx);
+        if(map[currChar-'a'] == true) {
+            //duplicate
+            removeDuplicates(str, idx+1, newStr, map);
+        } else {
+            map[currChar-'a'] = true;
+            removeDuplicates(str, idx+1, newStr.append(currChar), map);
+        }
+    }
+
+    public static int friendsPairing(int n) {
+        //base case
+        if (n == 1 || n == 2) {
+            return n;
+        }
+
+        return friendsPairing(n-1) + (n-1) * friendsPairing(n-2);
+    }
+
+    public static void printBinString(int n, int lastPlace, String str) {
+        //base case
+        if (n == 0) {
+            System.out.println(str);
+            return;
+        }
+        
+        //kaam
+        printBinString(n-1, 0, str + "0");
+        if(lastPlace == 0) {
+            printBinString(n-1, 1, str + "1");
+        }
+    }
+
 
     public static void main(String args[]) {
+
+        printBinString(3, 0,"");
+
+
+
+
+        // System.out.println(friendsPairing(5));
+
+        // String str = "appnnacollege";
+        // removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+
+        // System.out.println(tilingProblem(3));
+
+
         // int n = 11;
 
         // printDec(n);
@@ -95,10 +179,13 @@ public class RecursionBasic {
         // System.out.println(fib(n));
 
         // int arr[] = {1, 2, 3, 4, 5};  //true
-        int arr[] = {1, 2, 11, 8, 6, 11, 5};   //false
+        // int arr[] = {1, 2, 11, 8, 6, 11, 5};   //false
         // int arr[] ={11};
         // System.out.println(isSorted(arr, 0));
         // System.out.println(firstOcc(arr, 11, 0));
-        System.out.println(lastOccurence(arr, 11, 0));
+        // System.out.println(lastOccurence(arr, 11, 0));
+        // System.out.println(power(2, 5));
     }
 }
+
+ 
