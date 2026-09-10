0fc8cbd0509fd60a023bf58d1bd31da1e8570218
dell <pagaredivyanka@gmail.com>
2026-09-10 17:11:43 +0530

    approach 2 : O(n)

--- trees/Class.java
+++ trees/Class.java
@@ -1,6 +1,6 @@
 package trees;
 
-public class HeightOfTree {
+public class Class {
     static class Node {
         int data;
         Node left, right;
@@ -43,15 +43,16 @@ public class HeightOfTree {
         int rightSum = sum(root.right);
         return leftSum + rightSum + root.data;
     }
+// ********************************************************************
 
-    public static int diameter(Node root) {
+    public static int diameter2(Node root) { //O(n^2)
         if(root == null) {
             return 0;
         }
 
-        int leftDiam = diameter(root.left);
+        int leftDiam = diameter2(root.left);
         int leftHt = height(root.left);
-        int rightDiam = diameter(root.right);
+        int rightDiam = diameter2(root.right);
         int rightHt = height(root.right);
 
         int selfDiam = leftHt + rightHt + 1;
@@ -59,7 +60,27 @@ public class HeightOfTree {
         return Math.max(selfDiam, Math.max(leftDiam, rightDiam));
     }
 
+    static class Info {
+        int diam;
+        int ht;
+        public Info(int diam, int ht) {
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
 
+    public static Info diameter(Node root) { //O(n)
+        if (root == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+        return new Info(diam, ht);
+    }
     public static void main(String args[]) {
 
         /*
@@ -83,6 +104,7 @@ public class HeightOfTree {
        System.out.println(count(root)); //7
        System.out.println(sum(root)); //28
 
-       System.out.println(diameter(root)); //5
+       System.out.println(diameter2(root)); //5
+       System.out.println(diameter(root).diam); //5
     }
 }
