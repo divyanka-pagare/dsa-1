4fc0fa7b315ca13a945dc01fe322653998fa149a
dell <pagaredivyanka@gmail.com>
2026-09-22 23:56:00 +0530

    commit

--- BST.java
+++ BST.java
@@ -34,6 +34,20 @@ public class BST {
         System.out.print(root.data+" ");
         inorder(root.right);
     }
+
+    public static boolean search(Node root, int key) {
+        if(root == null) {
+            return false;
+        }
+
+        if(root.data == key) {
+            return true;
+        }
+
+        if(root.data > key) {
+            search(root.left, key);
+        }
+    }
     public static void main(String args[]) {
         int values[] = {5, 1, 3, 4, 2, 7};
         Node root = null;
@@ -44,5 +58,11 @@ public class BST {
 
         inorder(root);
         System.out.println();
+
+        if (search(root, 1)) {
+            System.out.println("found");
+        } else {
+            System.out.println("not found");
+        }
     }
 }
